cec07af29df155bf88633099a411f669ff6088b0
lucaszinhi <lucasdevjiparo@gmail.com>
2026-02-10 14:45:55 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const sections = document.querySelectorAll("section");
+
+window.addEventListener("scroll", () => {
+  sections.forEach(sec => {
+    const top = sec.getBoundingClientRect().top;
+    const screen = window.innerHeight;
+
+    if (top < screen - 100) {
+      sec.classList.add("show");
+    }
+  });
+});
